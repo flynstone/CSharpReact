10bb2be2ed60c0db7e5f3fc1d8b005d66edbc05e
Julien <flynstone@live.ca>
2022-03-25 12:00:26 -0400

    fixed dashboard

--- client-app/src/features/articles/dashboard/ArticleDashboard.tsx
+++ client-app/src/features/articles/dashboard/ArticleDashboard.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import ArticleList from "./ArticleList";
-import ArticleDetails from "../details/ArticleDetails";
-import ArticleForm from "../form/ArticleForm";
 import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
@@ -9,7 +7,6 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ArticleDashboard() {
   const { articleStore } = useStore();
-  const { selectedArticle, editMode } = articleStore;
   
   useEffect(() => {
     articleStore.loadArticles();
@@ -23,13 +20,7 @@ export default observer(function ArticleDashboard() {
       </Grid.Column>
 
       <Grid.Column width="6" className="pt-3">
-        {/* Double ampersand => used to generate component on the right side of the symbole as long as the left side is not null */}
-        {selectedArticle && !editMode && (
-          <ArticleDetails />
-        )}
-        {editMode && (
-          <ArticleForm />
-        )}
+        <h2>Article filters</h2>
       </Grid.Column>
     </Grid>
   );
