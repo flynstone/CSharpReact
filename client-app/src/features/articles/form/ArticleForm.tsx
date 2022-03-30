09ad3c14863f721c4dda0183e9c399f813e4344b
Julien <flynstone@live.ca>
2022-03-30 13:07:14 -0400

    updated form to use error validations

--- client-app/src/features/articles/form/ArticleForm.tsx
+++ client-app/src/features/articles/form/ArticleForm.tsx
@@ -1,13 +1,14 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
-import { Button, FormField } from "semantic-ui-react";
+import { Button, FormField, Label } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 import { v4 as uuid } from "uuid";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Article } from "../../../app/models/article";
 import * as Yup from 'yup';
+import { Card } from "@mui/material";
 
 export default observer(function ArticleForm() {
   const history = useHistory();
@@ -59,15 +60,33 @@ export default observer(function ArticleForm() {
         initialValues={article}
         onSubmit={(values) => console.log(values)}>
         {({ handleSubmit }) => (
-          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-            <FormField>
-              <Field name="title" placeholder="Title" />
-            </FormField>
+          <Form onSubmit={handleSubmit} autoComplete="off">
+            <Card className="Container">
+              <div className="Row">
+                <FormField>
+                  <Field style={{ backgroundColor: '#343434', color: 'beige', borderStyle: 'none' }} name="title" placeholder="Title" />
+                  <ErrorMessage
+                    name='title'
+                    render={error =>
+                      <Label pointing='left' style={{ backgroundColor: '#343434', color: 'red', borderStyle: 'none', fontSize: '18px' }} content={error} />
+                    }
+                  />
+                </FormField>
+                
+                <Field style={{backgroundColor: '#343434', color: 'beige', borderStyle: 'none'}} placeholder="Date" name="dateCreated" />
+              </div>
+
+              
+              <FormField className="py-5 px-3" >
+                <Field name="body" style={{display: 'flex', minWidth: '100%', backgroundColor: '#343434', color: 'beige', borderStyle: 'none'}} type="textarea" placeholder="Input content here" />
+              </FormField>
+              
+              <FormField className="px-5 Btn">
+                <Button basic color="blue" loading={loading} type="submit" floated="right">Submit</Button>{" "}
+                <Button as={Link} to="/articles" basic color="orange" type="button" floated="right">Cancel</Button>
+              </FormField>         
+            </Card>
             
-            <Field name="body" type="textarea" placeholder="Input content here" />
-            <Field placeholder="Date" name="dateCreated" />
-            <Button basic color="blue" loading={loading} type="submit" floated="right">Submit</Button>{" "}
-            <Button as={Link} to="/articles" basic color="orange" type="button" floated="right">Cancel</Button>
           </Form>
         )}
       </Formik>
