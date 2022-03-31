d3dfaac2c7f615cd283978a3a05a9a4378d781f7
Julien <flynstone@live.ca>
2022-03-31 12:09:46 -0400

    added user seed and modified program file

---
+++ CSharpReact.Data/Seeds/SeedUserData.cs
@@ -0,0 +1,40 @@
+﻿using CSharpReact.Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpReact.Data.Seeds
+{
+    public class Seed
+    {
+       public static async Task SeedUserData(AppDbContext context, UserManager<AppUser> userManager)
+        {
+            // If no users exist in the database, create
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    // Create new user
+                    new AppUser
+                    {
+                        DisplayName = "Guest",
+                        UserName = "guest",
+                        Email = "guest@test.com",
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Test",
+                        UserName = "test",
+                        Email = "test@mail.com"
+                    }
+                };
+
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "P@ssw0rd");
+                }
+            }
+        }
+    }
+}
