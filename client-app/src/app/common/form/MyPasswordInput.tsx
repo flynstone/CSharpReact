031d174bfca072a34951084e394121ced0524170
Julien <flynstone@live.ca>
2022-04-01 11:44:50 -0400

    fixed styling and login

--- client-app/src/app/common/form/MyPasswordInput.tsx
+++ client-app/src/app/common/form/MyPasswordInput.tsx
@@ -5,15 +5,16 @@ import { Form, Label } from "semantic-ui-react";
 interface Props {
   placeholder: string;
   name: string;
+  type?: string;
   label?: string;
 }
 
-export default function MyTextInput(props: Props) {
+export default function MyPasswordInput(props: Props) {
   const [field, meta] = useField(props.name);
   return (
     <Form.Field error={meta.touched && !!meta.error}>
       <label>{props.label}</label>
-      <input {...field} {...props} style={{backgroundColor: '#343434', color: 'beige', borderStyle: 'none'}} />
+      <input {...field} {...props} type='password' style={{backgroundColor: '#343434', color: 'beige', borderStyle: 'none'}} />
       {meta.touched && meta.error ? (
         <Label basic style={{ backgroundColor: '#343434', color: 'red', borderStyle: 'none', fontSize: '18px' }}>{meta.error}</Label>
       ): null}
