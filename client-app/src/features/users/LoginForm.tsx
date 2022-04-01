f0ea24475d6484eeb513b44682db367f6fe27995
Julien <flynstone@live.ca>
2022-04-01 12:18:32 -0400

    implemented login on client-side

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -17,12 +17,10 @@ export default observer(function LoginForm() {
       >
         {({ handleSubmit, isSubmitting, errors }) => ( 
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-          <Card className="Container">
-            <div>
-              <MyTextInput name="email" placeholder="Email" />
-            </div>
-             
-              <MyPasswordInput name="password" placeholder="Password" type="password" />
+          <Card className="Container">        
+            <MyTextInput name="email" placeholder="Email" />            
+            <MyPasswordInput name="password" placeholder="Password" type="password" />
+       
               <ErrorMessage   
                 name='error' render={() => <Label basic style={{ marginBottom: 10, marginTop: 10, backgroundColor: '#343434', color: 'red', borderStyle: 'none', fontSize: '18px' }} content={errors.error} />}
               />  
