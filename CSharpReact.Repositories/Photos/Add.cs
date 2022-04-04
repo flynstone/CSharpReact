0f72385535bb242ef38e8388093150f6cb7af894
Julien <flynstone@live.ca>
2022-04-04 11:11:41 -0400

    added Photo mediator

---
+++ CSharpReact.Repositories/Photos/Add.cs
@@ -0,0 +1,62 @@
+﻿using CSharpReact.Data;
+using CSharpReact.Entities.Models;
+using CSharpReact.Repositories.Core;
+using CSharpReact.Repositories.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpReact.Repositories.Photos
+{
+    public class Add
+    {
+        public class Command : IRequest<Result<Photo>>
+        {
+            public IFormFile File { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Photo>>
+        {
+            private readonly AppDbContext _context;
+            private readonly IPhotoAccessor _photoAccessor;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(AppDbContext context, IPhotoAccessor photoAccessor, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _photoAccessor = photoAccessor;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Photo>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(p => p.Photos)
+                    .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                // Handle null request
+                if (user == null) return null;
+
+                var photoUploadResult = await _photoAccessor.AddPhoto(request.File);
+
+                var photo = new Photo
+                {
+                    Url = photoUploadResult.Url,
+                    Id = photoUploadResult.PublicId
+                };
+
+                // If the photo is the first photo uploaded by user, set it to "Main"
+                if (!user.Photos.Any(x => x.IsMain)) photo.IsMain = true;
+
+                // Add the photo and save
+                user.Photos.Add(photo);
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (result) return Result<Photo>.Success(photo);
+
+                return Result<Photo>.Failure("There was a problem adding photo");
+            }
+        }
+    }
+}
