3b9ccea768378975fcc10f4159a7eaad4d1a01ba
Julien <flynstone@live.ca>
2022-04-05 14:13:01 -0400

    added create handler for comments table

---
+++ CSharpReact.Repositories/Comments/Create.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using CSharpReact.Data;
+using CSharpReact.Entities.Models;
+using CSharpReact.Repositories.Core;
+using CSharpReact.Repositories.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpReact.Repositories.Comments
+{
+    public class Create
+    {
+        public class Command : IRequest<Result<CommentDto>>
+        {
+            public string Message { get; set; }
+            public Guid ArticleId { get; set; }
+
+        }
+
+        // Using FluentValidation
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Message).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<CommentDto>>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _mapper = mapper;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                // Get Article => store it in variable article
+                var article = await _context.Articles.FindAsync(request.ArticleId);
+
+                // Handle null request
+                if (article == null) return null;
+
+                var user = await _context.Users
+                    .Include(p => p.Photos)
+                    .SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                var comment = new Comment
+                {
+                    Author = user,
+                    Article = article,
+                    Message = request.Message,
+                };
+
+                article.Comments.Add(comment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment));
+
+                return Result<CommentDto>.Failure("There was a problem adding the comment");
+            }
+        }
+    }
+}
