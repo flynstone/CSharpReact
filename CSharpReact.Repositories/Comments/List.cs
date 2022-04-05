4e985f51d69d9e914081e4c8c6a0fce273b8731c
Julien <flynstone@live.ca>
2022-04-05 14:19:20 -0400

    added list handler for comments

---
+++ CSharpReact.Repositories/Comments/List.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CSharpReact.Data;
+using CSharpReact.Repositories.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpReact.Repositories.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentDto>>> 
+        { 
+            public Guid ArticleId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+        {
+            private readonly AppDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(AppDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<List<CommentDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.Comments
+                    .Where(x => x.Article.Id == request.ArticleId)
+                    .OrderBy(x => x.CreateAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentDto>>.Success(comments);
+            }
+        }
+    }
+}
